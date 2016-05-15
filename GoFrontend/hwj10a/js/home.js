b364011d59fcc8edd8b64f926c88046e6764d924
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2016-05-15 19:36:27 +0300

    JavaScript Lessons 19-20 Second preview

--- GoFrontend/hwj10a/js/home.js
+++ GoFrontend/hwj10a/js/home.js
@@ -2,31 +2,40 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-  /*
-  * Render Benefits Section
-  */
-  var lodashCompiledTemplate = _.template(document
-    .getElementById("benefit-template").innerHTML, {variable: "benefits"});
-  var element = document.querySelector("section.benefits > div.container");
-  var benefits = [
-    {
-      title: "Power Inside",
-      text: "Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique"
-    },
-    {
-      title: "New Technologies",
-      text: "Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique"
-    },
-    {
-      title: "Fast Support",
-      text: "Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique"
-    }
-  ];
-  element.innerHTML += lodashCompiledTemplate(benefits);
+    /*
+    * Render Benefits Section
+    */
+    var benefits = [
+      {
+        title: "Power Inside",
+        text: "Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique"
+      },
+      {
+        title: "New Technologies",
+        text: "Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique"
+      },
+      {
+        title: "Fast Support",
+        text: "Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique"
+      }
+    ];
+    var lodashCompiledTemplate = _.template(document
+      .getElementById("benefit-template").innerHTML, {variable: "benefits"});
+    var element = document.querySelector("section.benefits > div.container");
+    element.innerHTML += lodashCompiledTemplate(benefits);
 
-  /*
-  * Render Services Section
-  */
-
+    /*
+    * Render Services Section
+    */
+    var services = [
+      'Schedule Services',
+      'Preventive Maintence',
+      'Tire & Wheel Services',
+      'Repair Services'
+    ];
+    lodashCompiledTemplate = _.template(document
+      .getElementById("services-template").innerHTML, {variable: "services"});
+    element = document.querySelector("section.services > div.container > div");
+    element.innerHTML += lodashCompiledTemplate(services);
 
 });
