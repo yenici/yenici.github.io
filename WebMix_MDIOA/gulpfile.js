65e16cf4d4259f58d5cf943c8477ac63c796caa5
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2016-07-11 09:00:09 +0300

    WebMix (Microsof, DropBox, Invision, Oculus, Apple ) initial commit

---
+++ WebMix_MDIOA/gulpfile.js
@@ -0,0 +1,198 @@
+'use strict';
+
+// gulp modules
+var gulp          = require('gulp'),
+    bowerfiles    = require('gulp-main-bower-files'),
+    flatten       = require('gulp-flatten'),
+
+    connect       = require('gulp-connect'),
+    plumber       = require('gulp-plumber'),
+    rename        = require('gulp-rename'),
+    rigger        = require('gulp-rigger'),
+    rimraf        = require('rimraf'),
+    sequence      = require('run-sequence'),
+    filter        = require('gulp-filter'),
+    sass          = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    cleancss      = require('gulp-clean-css'),
+    jshint        = require('gulp-jshint'),
+    jshintstylish = require('jshint-stylish'),
+    babel         = require('gulp-babel'),
+    uglify        = require('gulp-uglify'),
+    spritesmith   = require('gulp.spritesmith'),
+    imagemin      = require('gulp-imagemin'),
+    imageminpng   = require('imagemin-pngquant');
+
+// Connect server parameters
+var testServer = {
+  // host: '192.168.88.231',
+  host: '192.168.0.2',
+  port: 8080,
+  root: 'dist/'
+};
+
+// Pathes
+var path = {
+  source: {
+    views: 'source/views/',
+    styles: 'source/styles/',
+    images: 'source/images/',
+    scripts: 'source/scripts/'
+  },
+  dist: {
+    dist: 'dist/',
+    views: 'dist/',
+    styles: 'dist/styles/',
+    images: 'dist/images/',
+    scripts: 'dist/scripts/'
+  },
+  tmp: 'source/tmp/'
+};
+
+// Tasks
+gulp.task('clean:tmp', function (cb) {
+  // rimraf(path.tmp, cb);
+  return true;
+});
+
+gulp.task('clean:dist', function (cb) {
+  // rimraf(path.dist, cb);
+  return true;
+});
+
+// H T M L
+gulp.task('build:html', function() {
+  return gulp.src(path.source.views + '*.html')
+    .pipe(rigger())
+    .pipe(gulp.dest(path.dist.views))
+    .pipe(connect.reload());
+});
+
+// S t y l e s
+gulp.task('build:styles', function() {
+  return gulp.src(path.source.styles + '**/*.scss')
+    .pipe(sass().on('error', sass.logError), {
+      outputStyle: 'expanded',
+      indentType: 'space',
+      indentWidth: 2,
+      precision: 10
+    })
+    .pipe(autoprefixer({
+			browsers: ['last 2 versions', 'ie >= 8'],
+			cascade: true
+		}))
+    .pipe(gulp.dest(path.tmp))
+    // .pipe(sourcemaps.init())
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.dist.styles))
+    .pipe(connect.reload());
+});
+
+// S c r i p t s
+gulp.task('build:scripts', function() {
+  return gulp.src(path.source.scripts + "**/*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    // .pipe(sourcemaps.init())
+    // .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.dist.scripts))
+    .pipe(connect.reload());
+});
+
+// B o w e r   C o m p o n e n t s
+gulp.task('build:bower', function() {
+  const FILTER_JS = filter('**/*.js', {restore:  true});
+  const FILTER_CSS = filter('**/*.css', {restore:  true});
+  return gulp.src('./bower.json')
+    .pipe(bowerfiles())
+    .pipe(flatten())
+    .pipe(FILTER_JS)
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(path.dist.scripts))
+    .pipe(FILTER_JS.restore)
+    .pipe(FILTER_CSS)
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(path.dist.styles));
+});
+
+// I m a g e s
+gulp.task('build:images', ['build:sprites'],  function() {
+  const imagefilter = filter([
+    path.source.images + '*.jpg',
+    path.source.images + '*.png',
+    path.source.images + '*.svg'
+  ]);
+  return gulp.src(path.source.images + '*')
+    .pipe(imagefilter)
+    .pipe(imagemin({
+        interlaced: true,
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [imageminpng()]
+    }))
+    .pipe(gulp.dest(path.dist.images))
+    .pipe(connect.reload());
+});
+
+gulp.task('build:sprites', function() {
+  var spriteData = gulp.src(path.source.images + 'sprites/*.png')
+    .pipe(spritesmith({
+      algorithm: 'left-right',
+      cssFormat: 'scss_maps',
+      cssName: '_sprite.scss',
+      imgName: 'sprites.png',
+      imgPath: 'sprites.png'
+    }));
+    spriteData.img.pipe(gulp.dest(path.source.images));
+    spriteData.css.pipe(gulp.dest(path.source.styles + 'components/'));
+    return true;
+});
+
+gulp.task('build', function(callback) {
+  sequence(
+    // ['clean:tmp', 'clean:dist'],
+    ['build:html', 'build:scripts', 'build:styles', 'build:bower', 'build:images'],
+    callback
+  );
+});
+
+gulp.task('server', function() {
+  connect.server({
+    root: testServer.root,
+    host: testServer.host,
+    port: testServer.port,
+    livereload: true,
+    debug: true
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(path.source.views + '**/*.html', ['build:html']);
+  gulp.watch(path.source.styles + '**/*.scss', ['build:styles']);
+  gulp.watch(path.source.scripts + '**/*.js', ['build:scripts']);
+  gulp.watch(path.source.images, ['build:images']);
+});
+
+gulp.task('default', function() {
+  sequence(
+    'build',
+    ['server', 'watch']
+  );
+});
---
+++ WebMix_MDIOA/source/scripts/main.js
@@ -0,0 +1,237 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  /*-- ****************************   Header   *************************** --*/
+
+  var lodashCompiledTemplate = _.template(
+    document.getElementById('ms-header-template').innerHTML,
+    {variable: 'data'}
+  );
+  document.getElementById('ms-menu')
+    .insertAdjacentHTML('beforeend', lodashCompiledTemplate(getMsMenu()));
+  document.getElementById('ms-menu').addEventListener( 'click', e => {
+    if (~e.target.className.indexOf('ms-menu__item')) {
+      let subMenu = e.target.nextElementSibling;  // Looking for a submenu
+      let parent = e.target.parentElement.parentElement; // UL.ms-menu < LI < (A.ms-menu__item)
+      // Close all siblings and descendants
+      let isActive = ~e.target.className.indexOf('active');
+      let activeElements;
+      while((activeElements = parent.getElementsByClassName('active')).length > 0) {
+        if (activeElements[0].getAttribute('data-height')) { // Restore original height
+          activeElements[0].style.height = activeElements[0].getAttribute('data-height') + 'px';
+        }
+        activeElements[0].className = activeElements[0].className.replace(' active', '');
+      }
+      // Remove / set active status
+      if (isActive) {
+        if (parent.getAttribute('data-height')) { // Restore original height
+          parent.style.height = parent.getAttribute('data-height') + 'px';
+        }
+      } else {
+        e.target.className += ' active';
+        if (subMenu) {
+          subMenu.className += ' active';
+          if (~subMenu.className.indexOf('level3')) {
+            let parentHeight;
+            if (!parent.getAttribute('data-height')) {
+              parentHeight = parent.offsetHeight;
+              parent.setAttribute('data-height', parentHeight); // Store original height for the 2nd level ms-menu
+            } else {
+              parentHeight = parent.getAttribute('data-height');
+            }
+            if (parentHeight > subMenu.offsetHeight) {
+              subMenu.style.height = parentHeight + 'px';
+            } else {
+              parent.style.height = subMenu.offsetHeight + 'px';
+            }
+          }
+        }
+      }
+    }
+  });
+});
+
+function getMsMenu() {
+  return(
+    [
+      {
+        name: "Sign in",
+        link: "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=12&ct=1468141168&rver=6.5.6509.0&wp=MBI&wreply=https:%2F%2Fwww.microsoft.com%2Fen-us%2F&lc=1033",
+        children: []
+      },
+      {
+        name: "Store",
+        link: "javascript:void(0)",
+        children: [
+          {
+            name: "Store home",
+            link: "http://www.microsoftstore.com/store/msusa/en_US/home",
+            children: []
+          },
+          {
+            name: "Device",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Microsoft Surface", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/All-Surface/categoryID.69403400"},
+              {name: "PCs &amp; tablets", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/All-PCs-tablets/categoryID.69404700"},
+              {name: "Xbox", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/categoryID.69405400"},
+              {name: "Virtual reality", link: "https://www.microsoftstore.com/store/msusa/en_US/cat/Virtual-and-Augmented-Reality/categoryID.594470000?icid=en_US_Store_UH_devices_vr"},
+              {name: "Accessories", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/All-accessories/categoryID.69407900"},
+              {name: "Windows phone", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/All-Windows-Phone/categoryID.69406200"},
+              {name: "Microsoft Band", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/categoryID.69889600"}
+            ]
+          },
+          {
+            name: "Software",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Office", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/All-Office/categoryID.69403900"},
+              {name: "Windows", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/categoryID.70036700"},
+              {name: "Additional software", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/All-additional-software/categoryID.69407400"}
+            ]
+          },
+          {
+            name: "Apps",
+            link: "javascript:void(0)",
+            children: [
+              {name: "All apps", link: "https://www.microsoft.com/en-us/store/apps"},
+              {name: "Windows apps", link: "https://www.microsoft.com/en-us/store/apps/windows"},
+              {name: "Windows phone apps", link: "https://www.microsoft.com/en-us/store/apps/windows-phone"}
+            ]
+          },
+          {
+            name: "Games",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Xbox One games", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/Xbox-One-games/categoryID.69405600"},
+              {name: "Xbox 360 games", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/Xbox-360-games/categoryID.69405800"},
+              {name: "PC games", link: "http://www.microsoftstore.com/store/msusa/en_US/list/PC-games/categoryID.62688000?icid=en_US_Store_UH_games_PC"},
+              {name: "Windows games", link: "https://www.microsoft.com/en-us/store/games/windows"},
+              {name: "Windows phone games", link: "https://www.microsoft.com/en-us/store/games/windows-phone"}
+            ]
+          },
+          {
+            name: "Entertainment",
+            link: "javascript:void(0)",
+            children: [
+              {name: "All Entertainment", link: "https://www.microsoft.com/en-us/store/entertainment"},
+              {name: "Movies &amp; TV", link: "https://www.microsoft.com/en-us/store/movies-and-tv"},
+              {name: "Music", link: "https://www.microsoft.com/en-us/store/music"}
+            ]
+          },
+          {
+            name: "Business &amp; Education",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Business Store", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/Business/categoryID.69408700"},
+              {name: "Education Store", link: "http://www.microsoftstore.com/store/msusa/en_US/edu"},
+              {name: "Developer", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/Developer/categoryID.69418300?icid=en_US_Store_UH_BusEd_Dev"}
+            ]
+          },
+          {
+            name: "Sale",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Back-to-school essentials", link: "https://www.microsoftstore.com/store/msusa/en_US/cat/Back-to-school-essentials/categoryID.67998200?icid=en_US_Store_UH_sale_gm_bts",children: []},
+              {name: "Sale", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/Sale/categoryID.69403000"}
+            ]
+          },
+          {
+            name: "Find a store",
+            link: "http://www.microsoft.com/en-us/store/locations/find-a-store?icid=L_navAB_FindAStore",
+            children: []
+          },
+          {
+            name: "Gift cards",
+            link: "https://www.microsoft.com/en-us/store/gift-cards",
+            children: []
+          }
+        ]
+      },
+      {
+        name: "Products",
+        link: "javascript:void(0)",
+        children: [
+          {
+            name: "Software &amp; services",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Windows", link: "//www.microsoft.com/en-us/windows"},
+              {name: "Office", link: "https://products.office.com/en-us/home"},
+              {name: "Free downloads &amp; security", link: "https://www.microsoft.com/en-us/download/default.aspx"},
+              {name: "Internet Explorer", link: "http://windows.microsoft.com/en-us/internet-explorer/download-ie"},
+              {name: "Microsoft Edge", link: "http://www.microsoft.com/en-us/windows/microsoft-edge"},
+              {name: "Skype", link: "http://www.skype.com/en/"},
+              {name: "OneNote", link: "http://www.onenote.com/"},
+              {name: "OneDrive", link: "https://onedrive.live.com/about/en-us/"},
+              {name: "Microsoft Health", link: "http://www.microsoft.com/microsoft-health/en-us"},
+              {name: "MSN", link: "http://www.msn.com/?ocid=HEA000"},
+              {name: "Bing", link: "http://www.bing.com"},
+              {name: "Microsoft Groove", link: "https://www.microsoft.com/en-us/groove"},
+              {name: "Microsoft Movies &amp; TV", link: "https://www.microsoft.com/en-us/movies-and-tv"}
+            ]
+          },
+          {
+            name: "Devices &amp; Xbox",
+            link: "javascript:void(0)",
+            children: [
+              {name: "All Microsoft devices", link: "https://www.microsoft.com/devices/en-us"},
+              {name: "Microsoft Surface", link: "http://www.microsoft.com/surface/en-us"},
+              {name: "All Windows PCs &amp; tablets", link: "http://www.microsoftstore.com/store/msusa/en_US/cat/Computers/categoryID.62684600"},
+              {name: "PC accessories", link: "https://www.microsoft.com/accessories/en-us"},
+              {name: "Xbox &amp; games", link: "http://www.xbox.com"},
+              {name: "Microsoft Band", link: "http://www.microsoft.com/Microsoft-Band/en-us"},
+              {name: "Microsoft Lumia", link: "http://www.microsoft.com/en-us/mobile/"},
+              {name: "All Windows phones", link: "https://www.microsoft.com/en-us/windows/phones"},
+              {name: "Microsoft HoloLens", link: "http://www.microsoft.com/microsoft-hololens/en-us"}
+            ]
+          },
+          {
+            name: "For business",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Cloud Platform", link: "http://www.microsoft.com/en-us/server-cloud/"},
+              {name: "Microsoft Azure", link: "http://azure.microsoft.com/"},
+              {name: "Microsoft Dynamics", link: "http://www.microsoft.com/en-us/dynamics/default.aspx"},
+              {name: "Windows for business", link: "http://www.microsoft.com/en-us/windows/business/default.aspx"},
+              {name: "Office for business", link: "https://products.office.com/en-us/business/office"},
+              {name: "Skype for business", link: "https://products.office.com/en-us/skype-for-business"},
+              {name: "Surface for business", link: "http://www.microsoft.com/surface/en-us/business/overview"},
+              {name: "Enterprise solutions", link: "http://www.microsoft.com/enterprise"},
+              {name: "Small business solutions", link: "http://www.microsoft.com/en-us/business/"},
+              {name: "Find a solutions provider", link: "https://pinpoint.microsoft.com"},
+              {name: "Volume Licensing", link: "https://www.microsoft.com/en-us/licensing/default.aspx"}
+            ]
+          },
+          {
+            name: "For developers &amp; IT pros",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Develop Windows apps", link: "https://dev.windows.com/en-us"},
+              {name: "Microsoft Azure", link: "http://azure.microsoft.com/"},
+              {name: "MSDN", link: "http://msdn.microsoft.com/en-us/"},
+              {name: "TechNet", link: "http://technet.microsoft.com/en-us/"},
+              {name: "Visual Studio", link: "http://www.visualstudio.com/"}
+            ]
+          },
+          {
+            name: "For students &amp; educators",
+            link: "javascript:void(0)",
+            children: [
+              {name: "Office for students", link: "http://www.microsoft.com/en-us/education/products/office/default.aspx"},
+              {name: "OneNote in classroom", link: "http://www.microsoft.com/en-us/education/products/onenote/default.aspx"},
+              {name: "Shop PCs &amp; tablets perfect for students", link: "http://www.microsoftstore.com/store/msusa/en_US/edu"},
+              {name: "Microsoft in Education", link: "http://www.microsoft.com/en-us/education"}
+            ]
+          }
+        ]
+      },
+      {
+        name: "Support",
+        link: "https://support.microsoft.com/en-us",
+        children: []
+      }
+    ]
+  );
+}
