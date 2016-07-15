b99fda5da80a394913b3c1318bdbafe0cb556e2b
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2016-07-16 02:36:04 +0300

    Dropbox section is done. Some minor improvements

--- WebMix_MDIOA/source/scripts/main.js
+++ WebMix_MDIOA/source/scripts/main.js
@@ -56,6 +56,62 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  /*-- **************************   Section_Dp   ************************* --*/
+  document.getElementById('db-carousel__paginator').addEventListener('click', e => {
+    if (~e.target.className.indexOf('db-carousel__paginator-btn')) {
+      if (!~e.target.className.indexOf('active')) {
+        const page = parseInt(e.target.getAttribute('data-pg'));
+        setCurrentPage(page);
+      }
+    }
+  });
+  document.getElementById('db-carousel__navigation-arrow').addEventListener('click', e => {
+    setCurrentPage();
+  });
+  /**
+  * Function: setCurrentPage
+  *
+  * Parameters:
+  *    newPage     - the number of a selected page (0,1,2) or ignore this parameter to select the NEXT page
+  */
+  function setCurrentPage(newPage = -1) {
+    const slideNames = ['together', 'performance', 'safe']
+    const paginator = document.getElementById('db-carousel__paginator');
+    let currentPage = 0;
+    // Change active paginator button
+    const btns = paginator.getElementsByClassName('db-carousel__paginator-btn');
+    for(let i =0; i < btns.length; i++) {
+      if (~btns[i].className.indexOf('active')) {
+        toggleClass(btns[i], 'active');
+        currentPage = i;
+        break;
+      }
+    }
+    if (newPage < 0) {
+      newPage = (currentPage + 1) % 3;
+    }
+    toggleClass(btns[newPage], 'active');
+    // Change active screeen
+    document.getElementById('db-carousel__screen').className =
+      document.getElementById('db-carousel__screen').className.replace('--' + slideNames[currentPage], '--' + slideNames[newPage]);
+    const screen = document.getElementById('db-carousel__screen');
+    const images = screen.getElementsByClassName('db-carousel__screen-image');
+    toggleClass(images[currentPage], 'active');
+    setTimeout(() => {
+      const film = screen.getElementsByClassName('db-carousel__screen-film')[0];
+      film.setAttribute('style', 'transform: translateX(' + (-newPage * 100 / 3) + '%)');
+      toggleClass(images[newPage], 'active');
+    }, 600);
+    // Change active description
+    const textWrapper = document.getElementById('dp-carousel__text');
+    const texts = textWrapper.getElementsByClassName('dp-carousel__text-item');
+    toggleClass(texts[currentPage], 'active');
+    setTimeout(() => {
+      textWrapper.setAttribute('style', 'transform: translateX(' + (-newPage * 100 / 3) + '%)');
+      toggleClass(texts[newPage], 'active');
+    }, 450);
+  }
+
   /*-- **************************   Section_In   ************************* --*/
   const validator = {
     checkEmail: emailElement => {
